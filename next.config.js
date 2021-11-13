df74b750739f087f143accefab5c362b73ba57bc
jhon11390 <meljhon2@gmail.com>
2021-11-12 22:12:52 -0500

    add configuration in the project

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    loader: 'imgix',
+    path: '/',
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
