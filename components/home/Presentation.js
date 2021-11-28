568a02338d02113648a97008ae552b2a7004d29b
jhon11390 <meljhon2@gmail.com>
2021-11-28 10:29:00 -0500

    cambia letra principal a titillium

--- components/home/Presentation.js
+++ components/home/Presentation.js
@@ -13,7 +13,7 @@ function PresentationRody() {
     <Container id="start" style={{marginTop: "160px"}}>
       <Row className="row"> 
         <Col md="6"> 
-          <h1 className="color_letter display-4">RODY la aplicacion de transporte completa a tus necesidades...</h1>
+          <h1 className="color_letter display-4" style={{fontWeight: "bold"}}>RODY la aplicacion de transporte completa a tus necesidades...</h1>
           <p className="mb-5 mt-" style={{fontSize: "35px"}}>Es la alternativa de transporte a tu alcance, para mercancias, acarreos, transporte de personas. <br /> A un click de distancia.</p>
           <Button id="focusRody" variant="primary" size="lg" style={{backgroundColor: "rgb(193, 28, 62)", border: "rgb(193, 28, 62)", paddingRight: "40px", paddingLeft: "40px"}}><GoInfo href="#information">Mas informacion</GoInfo></Button>
         </Col>
--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="true"></link>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet"></link>
+          <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Montserrat:wght@300&family=Titillium+Web:wght@200&display=swap" rel="stylesheet"></link>
         </Head>
         
         <HeaderOption2 />
